71e227518c692e939e32652c5a2ef1ef22cd878c
Ehsan Heydary <58146358+wetdeveloper@users.noreply.github.com>
2022-01-10 05:46:41 -0500

    Add files via upload

---
+++ go-bd/app.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"text/template"
+
+	"github.com/labstack/echo/v4"
+)
+
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func main() {
+
+	e := echo.New()
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("*.html")),
+	}
+	e.Renderer = renderer
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "birthdateForm.html", map[string]interface{}{
+			"name": "Dolly!",
+		})
+	}).Name = "studentName"
+
+	e.POST("/birthdate", func(c echo.Context) error {
+		bd := c.FormValue("bd")
+		return c.String(http.StatusOK, bd)
+	}).Name = "birthdate"
+	e.Start(":")
+}
